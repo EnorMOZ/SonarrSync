780b11d19fa29e461dc47a4d40b93d9c14df0175
EnorMOZ <enormoz@gmail.com>
2018-03-31 06:41:13 -0400

    My attempt to python. Closer need to fix images.

--- RadarrSync.py
+++ RadarrSync.py
@@ -8,7 +8,7 @@ import configparser
 
 ########################################################################################################################
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
 
 fileHandler = logging.FileHandler("./Output.txt",'w','utf-8')
@@ -68,34 +68,38 @@ for movie in radarrMovies.json():
     for name, server in servers.items():
         if movie['profileId'] == int(server['profileidmatch']):
             if movie['tmdbId'] not in server['movies']:
-                path = str.replace(str(movie['path']), ConfigSectionMap("RadarrMaster")['basepath'], server['newpath'])
-                logging.debug('server: {0}'.format(name))
-                logging.debug('title: {0}'.format(movie['title']))
-                logging.debug('qualityProfileId: {0}'.format(server['profileid']))
-                logging.debug('titleSlug: {0}'.format(movie['titleSlug']))
-                images = movie['images']
-                for image in images:
-                    image['url'] = '{0}{1}'.format(radarr_url, image['url'])
-                    logging.debug(image['url'])
-                logging.debug('tmdbId: {0}'.format(movie['tmdbId']))
-                logging.debug('path: {0}'.format(path))
-                logging.debug('monitored: {0}'.format(movie['monitored']))
+                spath = ConfigSectionMap("RadarrMaster")['basepath']
+                logging.debug('spath: {0}'.format(spath))
+                if spath in movie['path']:
+                    rpath = str.replace(str(movie['path']), ConfigSectionMap("RadarrMaster")['basepath'], server['newpath'])
+                    logging.debug('server: {0}'.format(name))
+                    logging.debug('newpath: {0}'.format(rpath))
+                    logging.debug('title: {0}'.format(movie['title']))
+                    logging.debug('qualityProfileId: {0}'.format(server['profileid']))
+                    logging.debug('titleSlug: {0}'.format(movie['titleSlug']))
+                    images = movie['images']
+                    for image in images:
+                        image['url'] = '{0}{1}'.format(radarr_url, image['url'])
+                        logging.debug(image['url'])
+                    logging.debug('tmdbId: {0}'.format(movie['tmdbId']))
+                    logging.debug('path: {0}'.format(rpath))
+                    logging.debug('monitored: {0}'.format(movie['monitored']))
 
-                payload = {'title': movie['title'],
-                           'qualityProfileId': server['profileid'],
-                           'titleSlug': movie['titleSlug'],
-                           'tmdbId': movie['tmdbId'],
-                           'path': path,
-                           'monitored': movie['monitored'],
-                           'images': images,
-                           'profileId': movie['profileId'],
-                           'minimumAvailability': 'released'
-                           }
+                    payload = {'title': movie['title'],
+                               'qualityProfileId': server['profileid'],
+                               'titleSlug': movie['titleSlug'],
+                               'tmdbId': movie['tmdbId'],
+                               'path': rpath,
+                               'monitored': movie['monitored'],
+                               'images': images,
+                               'profileId': server['profileid'],
+                               'minimumAvailability': 'released'
+                               }
 
-                r = session.post('{0}/api/movie?apikey={1}'.format(server['url'], server['key']), data=json.dumps(payload))
-                logging.debug('payload: {0}'.format(payload))
-                server['searchid'].append(int(r.json()['id']))
-                logger.info('adding {0} to Radarr {1} server'.format(movie['title'], name))
+                    r = session.post('{0}/api/movie?apikey={1}'.format(server['url'], server['key']), data=json.dumps(payload))
+                    logging.debug('payload: {0}'.format(payload))
+                    server['searchid'].append(int(r.json()['id']))
+                    logger.info('adding {0} to Radarr {1} server'.format(movie['title'], name))
             else:
                 logging.debug('{0} already in {1} library'.format(movie['title'], name))
 
@@ -103,4 +107,3 @@ for name, server in servers.items():
     if len(server['searchid']):
         payload = {'name' : 'MoviesSearch', 'movieIds' : server['searchid']}
         session.post('{0}/api/command?apikey={1}'.format(server['url'], server['key']),data=json.dumps(payload))
-
